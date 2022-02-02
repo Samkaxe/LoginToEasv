c2b1df38ec3199e1c3183c8ebe5efffc213fbe44
Samkaxe <90753108+Samkaxe@users.noreply.github.com>
2022-02-02 17:13:00 +0100

    add animation :D

--- src/GUI/Controller/MainWindowController.java
+++ src/GUI/Controller/MainWindowController.java
@@ -1,5 +1,9 @@
 package GUI.Controller;
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,7 +13,10 @@ import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -19,28 +26,72 @@ public class MainWindowController {
     @FXML
     public ImageView image;
     @FXML
-    public Button login;
+    public Button signfxid;
     @FXML
-    public Button signin;
+    public StackPane stackpane;
+    @FXML
+    public AnchorPane mainancorpane;
+    @FXML
+    public Button logfxid;
 
     public void log(ActionEvent actionEvent) throws IOException {
-        FXMLLoader hero = new FXMLLoader(getClass().getResource("/GUI/View/login.fxml"));
-        Parent root = hero.load();
-        Stage stage = new Stage();
-        stage.setTitle("login");
-        stage.setScene(new Scene(root));
-        stage.show();
+        FXMLLoader kel = new FXMLLoader(getClass().getResource("/GUI/View/login.fxml"));
+        Parent root = kel.load();
+        Scene scene = logfxid.getScene();
+        root.translateYProperty().set(scene.getHeight());
+        stackpane.getChildren().add(root);
+        // stackpane.getChildren().remove(mainancorpane);
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(root.translateYProperty(), 0, Interpolator.EASE_IN);
+        KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.setOnFinished(t -> {
+            stackpane.getChildren().remove(mainancorpane);
+        });
+        timeline.play();
     }
 
     public void sign(ActionEvent actionEvent) throws IOException {
         FXMLLoader kel = new FXMLLoader(getClass().getResource("/GUI/View/sign.fxml"));
         Parent root = kel.load();
-        Stage stage = new Stage();
-        stage.setTitle("sign");
-        stage.setScene(new Scene(root));
-        stage.show();
+        Scene scene = signfxid.getScene();
+        root.translateYProperty().set(scene.getHeight());
+        stackpane.getChildren().add(root);
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(root.translateYProperty(), 0, Interpolator.EASE_IN);
+        KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.setOnFinished(t -> {
+            stackpane.getChildren().remove(mainancorpane);
+        });
+        timeline.play();
+       // Stage stage = new Stage();
+       // stage.setTitle("sign");
+      //  stage.setScene(new Scene(root));
+       // stage.show();
     }
+    /*
+     @FXML
+    private void handleNewSongbtn(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample/gui/view/AddEditSongView.fxml"));
+        Parent root = loader.load();
+      //  but.setGraphic(new ImageView("images/icon.png"));
+        but.setStyle("-fx-background-color: #26ff00; ");
+        Scene scene = but.getScene();
+        root.translateYProperty().set(scene.getHeight());
+        parentContainer.getChildren().add(root);
+        ((AddEditSongViewController)loader.getController()).setSongModel(songModel);
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(root.translateYProperty(), 0, Interpolator.EASE_IN);
+        KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.setOnFinished(t -> {
+            parentContainer.getChildren().remove(mainorderPain);
+        });
+        timeline.play();
+     */
         // easter
+    /*
     public void key(KeyEvent keyEvent) {
         ArrayList<KeyCode> cat = new ArrayList<>();
         cat.add(keyEvent.getCode());
@@ -59,6 +110,10 @@ public class MainWindowController {
 
          */
         // we could set new stage called tirminal with one text field in it and enter option
+
+
+
+    public void exit(ActionEvent actionEvent) {
 
     }
 }
--- src/GUI/Controller/SignController.java
+++ src/GUI/Controller/SignController.java
@@ -1,4 +1,41 @@
 package GUI.Controller;
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+import java.io.IOException;
+
 public class SignController {
+    @FXML
+    public Button bck;
+    @FXML
+    public StackPane stackpane;
+    @FXML
+    public AnchorPane anchorpane;
+
+    public void back(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/View/MainWindow.fxml"));
+        Scene scene = bck.getScene();
+        root.translateYProperty().set(scene.getWidth());
+        stackpane.getChildren().add(root);
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(root.translateYProperty(), 0, Interpolator.EASE_IN);
+        KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.setOnFinished(t -> {
+            stackpane.getChildren().remove(anchorpane);
+        });
+        timeline.play();
+    }
 }
